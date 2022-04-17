73349c2944adcd13050a1f87d8fe3d955398c562
hienm9 <98295316+hienm9@users.noreply.github.com>
2022-04-17 01:53:24 -0400

    added questions to the index.js git push

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,71 @@
-// TODO: Include packages needed for this application
+//Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-
-
+//Create an array of questions for user input to generate readme file
 const questions = [
     {
-        
+        type: "input",
+        name: "name",
+        message: "What's your name?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter your Github username?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "Enter GitHub link to your project?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What's your email?"
+    },
+    {
+        type: "input",
+        name: "title",
+        message: "What's the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Provide a description of your project:"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What license does your project have?",
+        choices: ["APACHE2.0", "ms-pl", "MIT", "zlib", "None"]
+    },
+    {
+        type: "input",
+        name: "dependencies",
+        message: "Any dependencies to install?",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "What commands do I need to run for the test?",
+        default: "npm test"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What does the usage of this repo?",
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Who are the contributors of this repo?",
     }
+
 ];
+      
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
